193880546a5bb580d0e29bd51f900988b511cd01
Mah Teck <mahteckteach@gmail.com>
2024-10-29 19:08:08 +0500

    remove bugs to deploy for versal

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -4,9 +4,9 @@ export default function NavBar(){
             <div className="container">
             <h1 className="logo">Johns Portfolio</h1>
             <ul className="nav-links">
-                <li><a href="/">Work</a></li>
-                <li><a href="/">Blog</a></li>
-                <li><a href="/">Contact</a></li>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
             </ul>
             </div>
         </nav>
