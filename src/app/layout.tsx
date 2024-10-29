1e313c168cc90de0e9d3e8115651e6cea17c7da4
Mah Teck <mahteckteach@gmail.com>
2024-10-29 19:03:31 +0500

    remove bugs to deploy in versal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,17 @@ import localFont from "next/font/local";
 import NavBar from "./component/Navbar";
 // import "./globals.css";
 import "./style/style.css";
-import Link from "next/link";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import { Session } from "inspector/promises";
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <>
